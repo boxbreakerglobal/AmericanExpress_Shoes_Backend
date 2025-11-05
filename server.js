25ff8b9d6cc6fd6306bed9acfe237c794be15020
BoxBreakerGlobal-Tech <108904244+boxbreakerglobal@users.noreply.github.com>
2025-11-05 22:41:56 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ dotenv.config()
 
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin:"*",
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
 // app.use(express.json())
 
 app.use(express.json());
